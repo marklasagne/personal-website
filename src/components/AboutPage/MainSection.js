ab2025814314ccf633b55a8ffe02034c777643da
Mark Lisanti <lisantimark@gmail.com>
2021-11-16 23:56:49 -0700

    meh

--- src/components/AboutPage/MainSection.js
+++ src/components/AboutPage/MainSection.js
@@ -1,6 +1,16 @@
+import Tilt from 'react-parallax-tilt';
+
+import portraitBackground from '../../assets/about/about_page_portrait_background.png';
+import portraitForeground from '../../assets/about/about_page_portrait.png';
+
 const AboutPage = () => {
     return (
       <>
+        <Tilt>
+      <div style={{ height: '300px', backgroundColor: 'darkgreen' }}>
+        <h1>React Parallax Tilt 👀</h1>
+      </div>
+    </Tilt>
         <h1>WOOOOOOOOO</h1>
       </>
     );
