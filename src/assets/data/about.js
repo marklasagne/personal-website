b4b8ce6ccd86cbc4e5acbb28064455a67e5837bf
Mark Lisanti <lisantimark@gmail.com>
2022-01-23 12:30:32 -0700

    refactor

--- src/assets/data/about.js
+++ src/assets/data/about.js
@@ -1,12 +1,12 @@
-export const core = "Howdy! name is Mark. I am currently a master’s student at the University of Colorado Boulder," +
-" pursuing a degree in Creative Technology and Design.  Being a lover of all things creative I am interested in the intersections" +
-" between art, design, and technology."
+export const core = "My name is Mark. I am currently a master’s student at the University of Colorado Boulder," +
+" pursuing a degree in Creative Technology and Design.  Tinkering with art, design and development and their intersections is what I like." +
+" Let's work on something cool?"
     
-export const work = "After graduating with a Bachelors in Computer Science I spent some time within IBM Hybrid Cloud as a software support" +
-" engineer.  Filling the gaps in my time I dabble with mobile and web development for personal and freelance projects.  As if I didn't spend enough" +
-" time on a computer I also enjoy many forms of digital art, specifically 2d and 3d animation and VFX as well as drawing and illustration."
+export const work = " After graduating with a Bachelors in Computer Science I spent some time within IBM Hybrid Cloud as a software support" +
+" engineer.  Now I have been filling the gaps in my time dabbling  with mobile and web development for personal and freelance projects,"  +
+" many of digital art, specifically 2d and 3d animation and VFX as well as drawing and illustration."
 
 export const extra = "Computers are definitely cool, but I promise I do leave my room." +
-" I enjoy mountain biking, climbing, and all the board sports. I am always down to chat" +
-" about anything so hit me up! Let's collaborate on something cool and or share how bad our kickflips are."
+" I HIGHLY value giving myself time to mountain biking, climbing, and all the board sports. " +
+" The faster and heavier the music the better.  ."
 
