fecd9fcd2e1ba92a2c17468751cb4eac6f0039c1
Mark <lisantimark@gmail.com>
2023-03-03 11:47:38 -0700

    slight change

--- src/pages/About.js
+++ src/pages/About.js
@@ -33,7 +33,7 @@ const About = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}>
             <h1>Howdy! I'm Mark</h1>
-            <h1 style={{fontSize: 12}}>[ software<span style={{fontStyle:'italic'}}>-icator</span> / art<span style={{fontStyle:'italic'}}>-gineer</span> / fabrication<span style={{fontStyle:'italic'}}>-ist</span> / anxiety ]</h1>
+            <h1 style={{fontSize: 12}}>[ software / art / fabrication / anxiety ]</h1>
             {AboutPageData.map((data, id) => {
               return(
               <Row key={id} style={{ zIndex: -1 }} data-aos='fade-up'>
@@ -52,7 +52,7 @@ const About = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}>
             <h1>Howdy! I'm Mark</h1>
-            <h1 style={{fontSize: 12}}>[ software<span style={{fontStyle:'italic'}}>-icator</span> / art<span style={{fontStyle:'italic'}}>-gineer</span> / fabrication<span style={{fontStyle:'italic'}}>-ist</span> / anxiety ]</h1>
+            <h1 style={{fontSize: 12}}>[ software / art / fabrication / anxiety ]</h1>
             <Portrait />
             {AboutPageData.map((data, id) => {
               return(
